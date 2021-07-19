0a3a9c7bc7610ed6d09408067a11dd7a79bdd0ba
ikhfhm <ikhwanfahmihidayat@gmail.com>
2021-07-19 11:23:26 +0700

    #5 | Create home page tablet

--- src/components/compilation/HomeCompilation.vue
+++ src/components/compilation/HomeCompilation.vue
@@ -1,18 +1,21 @@
 <template>
   <div>
     <HomeDesktop class="hidden lg:block" />
-    <HomeMobile class="lg:hidden" />
+    <HomeTablet class="hidden md:block lg:hidden" />
+    <HomeMobile class="md:hidden" />
   </div>
 </template>
 
 <script>
 import HomeDesktop from "@/components/desktop/view/HomeDesktop";
 import HomeMobile from "@/components/mobile/view/HomeMobile";
+import HomeTablet from "@/components/tablet/view/HomeTablet";
 
 export default {
   name: "HomeCompilation",
   components: {
     HomeDesktop,
+    HomeTablet,
     HomeMobile
   }
 };
---
+++ src/components/tablet/view/HomeTablet.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="h-screen font-ubuntu w-full bg-primary">
+    <HeaderTablet class="fixed z-10" />
+    <div class="absolute bottom-0 flex justify-center w-full">
+      <img src="../../../assets/ikhwan-desktop.png" class="h-screen pt-28" />
+    </div>
+  </div>
+</template>
+
+<script>
+import HeaderTablet from "@/components/tablet/HeaderTablet";
+
+export default {
+  name: "HomeTablet",
+  components: {
+    HeaderTablet
+  }
+};
+</script>
